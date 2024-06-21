c97066b7af7898ef1cd962392cddcc77d189ac8a
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2024-06-21 00:53:23 +0000

    test GeneracionEnergiaEnXHoras V2

--- tests/testsGeneracionEnergiaEnXHoras.test.ts
+++ tests/testsGeneracionEnergiaEnXHoras.test.ts
@@ -1,51 +1,32 @@
 import Reactor from "../src/reactor/reactor"
-import EstadoCriticidad from "../src/estados/estado-criticidad"
-import EstadoCritico from "../src/estados/estado-critico"
 import EstadoNormal from "../src/estados/estado-normal"
 
-it("Tests Punto 3i",()=>{
-    describe("Cantidad de energía neta producida en una determinada cantidad de horas. Considerar distintos escenarios",()=>{
+describe("Cantidad de energía neta producida en una determinada cantidad de horas. Considerar distintos escenarios",()=>{
+    it("Iniciando Reactor en EstadoNormal con una temp. de 280°, en 1 hora, genera 216.65 de energia neta",()=>{
+        const reactor = new Reactor(280,0,0,0,0,new EstadoNormal())
+        let energiaGenerada = reactor.energiaGeneradaEnXHoras(1);
+        energiaGenerada = Math.floor(energiaGenerada);
 
-        describe("En 1 hora", ()=>{
-            describe("Iniciando Reactor en EstadoNormal con una temp. de 280°, genera 216.65 de energia neta",()=>{
-                const energiaAGenerar = 216.65
-                const reactor = new Reactor(280,0,0,0,0,new EstadoNormal())
+        let energiaAGenerar = 299
+        
+        expect(energiaGenerada).toBe(energiaAGenerar);
+    })
+    it("Iniciando Reactor en EstadoNormal con una temp. de 280°, en 6 hora, genera 700 de energia neta",()=>{
+        const reactor = new Reactor(280,0,0,0,0,new EstadoNormal())
+        let energiaGenerada = reactor.energiaGeneradaEnXHoras(6);
+        energiaGenerada = Math.floor(energiaGenerada);
 
-                expect(reactor.energiaGeneradaEnXHoras(1)).toBe(energiaAGenerar);
-            })
-            describe("Iniciando Reactor en EstadoCriticidad con una temp. de 380°, genera x de energia neta",()=>{
-                const energiaAGenerar = 700
-                const reactor = new Reactor(380,0,0,0,0,new EstadoCriticidad())
+        const energiaAGenerar = 2799
+        
+        expect(energiaGenerada).toBe(energiaAGenerar);
+    })
+    it("Iniciando Reactor en EstadoNormal con una temp. de 280°, en 14 hora, genera 11198 de energia neta",()=>{
+        const reactor = new Reactor(280,0,0,0,0,new EstadoNormal())
+        let energiaGenerada = reactor.energiaGeneradaEnXHoras(15);
+        energiaGenerada = Math.floor(energiaGenerada);
 
-                expect(reactor.energiaGeneradaEnXHoras(1)).toBe(energiaAGenerar);
-            })
-            describe("Iniciando Reactor en EstadoCritico con una temp. de 410°, genera x de energia neta",()=>{
-                const energiaAGenerar = 700
-                const reactor = new Reactor(410,0,0,0,0,new EstadoCritico())
-
-                expect(reactor.energiaGeneradaEnXHoras(1)).toBe(energiaAGenerar);
-            })
-        })
- 
-        describe("En 6 horas", ()=>{
-            describe("Iniciando Reactor en EstadoNormal con una temp. de 280°, genera 700 de energia neta",()=>{
-                const energiaAGenerar = 700
-                const reactor = new Reactor(280,0,0,0,0,new EstadoNormal())
-                
-                expect(reactor.energiaGeneradaEnXHoras(6)).toBe(energiaAGenerar);
-            })
-            describe("Iniciando Reactor en EstadoCriticidad con una temp. de 380°, genera x de energia neta",()=>{
-                const energiaAGenerar = 700
-                const reactor = new Reactor(380,0,0,0,0,new EstadoCriticidad())
-                
-                expect(reactor.energiaGeneradaEnXHoras(6)).toBe(energiaAGenerar);
-            })
-            describe("Iniciando Reactor en EstadoCritico con una temp. de 410°, genera x de energia neta",()=>{
-                const energiaAGenerar = 700
-                const reactor = new Reactor(380,0,0,0,0,new EstadoCritico())
-
-                expect(reactor.energiaGeneradaEnXHoras(6)).toBe(energiaAGenerar);
-            })
-        })
+        const energiaAGenerar = 11198 
+        
+        expect(energiaGenerada).toBe(energiaAGenerar);
     })
 })
