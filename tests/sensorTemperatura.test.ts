4e3503a06cb504fa9c1e4a1d34cddd01b91f4419
B.Mateo <75537669+NarDevGH@users.noreply.github.com>
2024-06-21 22:44:03 +0000

    arreglar throw EmisorDeTemperaturaNoSeteado en SensorTemperatura y Test

--- tests/sensorTemperatura.test.ts
+++ tests/sensorTemperatura.test.ts
@@ -9,7 +9,7 @@ describe("Tests Sensor de Temperatura",()=>{
         estadoNormal = new EstadoNormal();
     })
 
-    describe("Obtener emisor de temperatura.",()=>{
+    test("Obtener emisor de temperatura.",()=>{
         const temperaturaDelReactor = 304.99;
         let reactor = new Reactor(temperaturaDelReactor,0,0,0,0,estadoNormal);
         let sensorTemperatura = new SensorTemperatura(reactor);
@@ -17,13 +17,13 @@ describe("Tests Sensor de Temperatura",()=>{
         expect(sensorTemperatura.getEmisorTemperatura()).toBe(reactor);
     })
 
-    describe("Throw EmisorDeTemperaturaNoSeteado al obtener el emisor de temperatura sin estar seteado.",()=>{
+    test("Throw EmisorDeTemperaturaNoSeteado al obtener el emisor de temperatura sin estar seteado.",()=>{
         let sensorTemperatura = new SensorTemperatura();
         
-        expect(sensorTemperatura.getEmisorTemperatura()).toThrow(EmisorDeTemperaturaNoSeteado);
+        expect(() => {sensorTemperatura.getEmisorTemperatura()}).toThrow(new EmisorDeTemperaturaNoSeteado());
     })
 
-    describe("Cambiar el emisor de temperatura.",()=>{
+    test("Cambiar el emisor de temperatura.",()=>{
         const temperaturaDelReactor = 304.99;
         let reactor = new Reactor(temperaturaDelReactor,0,0,0,0,estadoNormal);
         let sensorTemperatura = new SensorTemperatura(reactor);
@@ -35,7 +35,7 @@ describe("Tests Sensor de Temperatura",()=>{
         expect(sensorTemperatura.getEmisorTemperatura()).toBe(reactor2);
     })
 
-    describe("Obtener lectura del emisor de temperatura.",()=>{
+    test("Obtener lectura del emisor de temperatura.",()=>{
         const temperaturaDelReactor = 304.99;
         let reactor = new Reactor(temperaturaDelReactor,0,0,0,0,estadoNormal);
         let sensorTemperatura = new SensorTemperatura(reactor);
@@ -43,9 +43,9 @@ describe("Tests Sensor de Temperatura",()=>{
         expect(sensorTemperatura.lecturaDeTemperatura()).toBe(temperaturaDelReactor);
     })
 
-    describe("Throw EmisorDeTemperaturaNoSeteado al leer temperatura del emisor de temperatura sin estar seteado.",()=>{
+    test("Throw EmisorDeTemperaturaNoSeteado al leer temperatura del emisor de temperatura sin estar seteado.",()=>{
         let sensorTemperatura = new SensorTemperatura();
         
-        expect(sensorTemperatura.lecturaDeTemperatura()).toThrow(EmisorDeTemperaturaNoSeteado);
+        expect(() => {sensorTemperatura.lecturaDeTemperatura()}).toThrow(new EmisorDeTemperaturaNoSeteado());
     })
 })
